ee0a818163ad1d987108d768ba7649dddd3a6204
Benjamin Lee <benjaminlee314@icloud.com>
2026-03-13 13:15:02 -0700

    Initial Commit

---
+++ LS-EENDTest/LS_EENDTestApp.swift
@@ -0,0 +1,32 @@
+//
+//  LS_EENDTestApp.swift
+//  LS-EENDTest
+//
+//  Created by Benjamin Lee on 3/13/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LS_EENDTestApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
